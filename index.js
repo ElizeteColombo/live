8f9da90bf29cf855ace3038e25431bd55052de85
Elizete Colombo <elizetecolombo@gmail.com>
2020-04-05 13:44:55 -0300

    chore: altear retorno da rota

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express');
 
 const app = express();
 app.get('/teste',(reg,res) => {
-    return res.json({hello: 'word'})
+    return res.json({hello: 'word 2'})
 });
 app.listen(3333);
